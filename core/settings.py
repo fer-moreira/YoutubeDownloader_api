566c29e8d433db272207fa3e8d63b718a8e6b9f3
Fernando Moreira <oakley-hbs@hotmail.com>
2021-03-09 23:30:03 -0300

    added heroku files

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -50,6 +52,9 @@ INSTALLED_APPS = [
     # APPS
     'apps.api',
 ]
+
+django_heroku.settings(locals())
+
 
 MIDDLEWARE = [
     # CORS
